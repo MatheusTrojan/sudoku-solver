9c877b10994d9496fffd3174050cc0039522469e
Matheus Trojan <matheustrojan23@gmail.com>
2022-10-07 11:05:45 -0300

    Adicionando mudança de temas

---
+++ src/scripts/tema.js
@@ -0,0 +1,14 @@
+function temaOriginal() {
+    document.body.classList.remove("dark")
+    document.body.classList.remove("azul")
+}
+
+function temaDark() {
+    document.body.classList.add("dark")
+    document.body.classList.remove("azul")
+}
+
+function temaAzul() {
+    document.body.classList.add("azul")
+    document.body.classList.remove("dark")
+}
